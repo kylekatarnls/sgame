2e2ef20e46b3be4869b546804d04e3ba193b4dd4
Bastien <kylekatarnls@github.com>
2013-12-22 16:59:25 +0100

    Optimisation de l'import CSV

--- app/database/seeds/CrawledContentSeeder.php
+++ app/database/seeds/CrawledContentSeeder.php
@@ -9,16 +9,11 @@ class CrawledContentSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		$file = explode("\n", file_get_contents(substr(__FILE__, 0, -4).'.csv'));
-		$headers = array_map('trim', explode("\t", trim($file[0])));
-		unset($file[0]);
-		foreach ($file as $line)
+		$file = fopen(substr(__FILE__, 0, -4).'.csv', 'r');
+		$headers = fgetcsv($file, 1024, ';');
+		while($line = fgetcsv($file, 1024, ';'))
 		{
-			$line = array_map('trim', explode("\t", trim($line)));
-			if(count($headers) === count($line))
-			{
-				CrawledContent::create(array_combine($headers, $line));
-			}
+			CrawledContent::create(array_combine($headers, $line));
 		}
 	}
 
