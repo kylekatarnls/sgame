55dcd8e63078a60194544e7b77dc1098777772f8
Bastien <kylekatarnls@github.com>
2013-12-27 00:44:23 +0100

    Debug test pour la preprod

--- app/models/EmulateMemcachedWithMemcache.php
+++ app/models/EmulateMemcachedWithMemcache.php
@@ -2,37 +2,34 @@
 
 if(!class_exists('Memcache') && class_exists('Memcached'))
 {
-	namespace
+	class \Memcached extends Memcached
 	{
-		class Memcached extends Memcached
+		protected $resultCode = -1;
+
+		public function get($key)
 		{
-			protected $resultCode = -1;
+			$flags = false;
+			$value = parent::get($key, $flags);
 
-			public function get($key)
-			{
-				$flags = false;
-				$value = parent::get($key, $flags);
+			// if $flags has been touched, key was found
+			// http://php.net/manual/fr/memcache.get.php#112056
+			$resultCode = ($flags !== false ? 0 : -1);
+			return $value;
+		}
 
-				// if $flags has been touched, key was found
-				// http://php.net/manual/fr/memcache.get.php#112056
-				$resultCode = ($flags !== false ? 0 : -1);
-				return $value;
-			}
+		public function getResultCode()
+		{
+			return $this->resultCode;
+		}
 
-			public function getResultCode()
-			{
-				return $this->resultCode;
-			}
-
-			public function set($key, $value, $seconds)
-			{
-				parent::set(
-					$this->prefix.$key,
-					$value,
-					strlen($value) > 512 ? MEMCACHE_COMPRESSED : 0,
-					$seconds
-				);
-			}
+		public function set($key, $value, $seconds)
+		{
+			parent::set(
+				$this->prefix.$key,
+				$value,
+				strlen($value) > 512 ? MEMCACHE_COMPRESSED : 0,
+				$seconds
+			);
 		}
 	}
 }
