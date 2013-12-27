e4d692443da34f1701d4e2c4fe32a1f8446fbbc2
Bastien <kylekatarnls@github.com>
2013-12-27 01:02:45 +0100

    Débogage de l'émulation de Memcached

--- app/models/EmulateMemcachedWithMemcache.php
+++ app/models/EmulateMemcachedWithMemcache.php
@@ -10,6 +10,7 @@ if(!class_exists('Memcached') && class_exists('Memcache'))
 		{
 			$flags = false;
 			$value = parent::get($key, $flags);
+			$value = is_array($key) ? array_map('unserialize', $value) : unserialize($value);
 
 			// if $flags has been touched, key was found
 			// http://php.net/manual/fr/memcache.get.php#112056
@@ -24,10 +25,11 @@ if(!class_exists('Memcached') && class_exists('Memcache'))
 
 		public function set($key, $value, $seconds)
 		{
+			$value = is_array($key) ? array_map('serialize', $value) : serialize($value);
 			parent::set(
 				$key,
 				$value,
-				strlen($value) > 512 ? MEMCACHE_COMPRESSED : 0,
+				(is_array($value) ? array_sum(array_map('strlen', $value)) : strlen($value)) > 512 ? MEMCACHE_COMPRESSED : 0,
 				$seconds
 			);
 		}
