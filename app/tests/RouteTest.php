08300e331a7e18e8c71bd9e29a4caa8a8fee2974
OpenShift guest <kylekatarnls@gmail.com>
2014-02-02 14:55:33 -0500

    Test unitaire de la route /autocomplete avec des paramètres

--- app/tests/RouteTest.php
+++ app/tests/RouteTest.php
@@ -18,7 +18,7 @@ class RouteTest extends TestCase {
 		$this->assertFound('GET', '/most-popular/1/100');
 		$this->assertFound('GET', '/history/1');
 
-		$this->assertJsonResponse('POST', '/autocomplete');
+		$this->assertJsonResponse('POST', '/autocomplete', array('q' => 'p'));
 
 	}
 
